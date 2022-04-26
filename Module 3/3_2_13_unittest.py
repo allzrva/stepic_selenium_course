84e6c4d110ed4f265a273ff915b6e189df749d22
Alexandra E <alexandra_elizarova@hotmail.com>
2022-04-26 12:07:09 +0300

    Test case for unittest usage

---
+++ Module 3/3_2_13_unittest.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import unittest
+
+
+class Test3_2_13(unittest.TestCase):
+    def test_positive_test_case(self):
+        browser = webdriver.Chrome('C:\\chromedriver\\chromedriver.exe')
+        try:
+            link = "http://suninjuly.github.io/registration1.html"
+            browser.get(link)
+            welcome_text = self.verify_registration(browser)
+            self.assertEqual(welcome_text, "Congratulations! You have successfully registered!",
+                             f"Expected text: Congratulations! You have successfully registered!, Actual text: {welcome_text}")
+        finally:
+            time.sleep(2)
+            browser.quit()
+
+    def test_negative_test_case(self):
+        browser = webdriver.Chrome('C:\\chromedriver\\chromedriver.exe')
+        try:
+            link = "http://suninjuly.github.io/registration2.html"
+            browser.get(link)
+            welcome_text = self.verify_registration(browser)
+            self.assertEqual(welcome_text, "Congratulations! You have successfully registered!",
+                             f"Expected text: Congratulations! You have successfully registered!, Actual text: {welcome_text}")
+        finally:
+            time.sleep(2)
+            browser.quit()
+
+    @staticmethod
+    def verify_registration(browser):
+        # fill names and e-mail
+        browser.find_element(By.CSS_SELECTOR, '.first_block .first').send_keys('John')
+        browser.find_element(By.CSS_SELECTOR, '.first_block .second').send_keys('Doe')
+        browser.find_element(By.CSS_SELECTOR, '.first_block .third').send_keys('john.d@mailbox.com')
+        # submit
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+        time.sleep(2)
+        return browser.find_element(By.TAG_NAME, "h1").text
+
+
+if __name__ == "__main__":
+    unittest.main()
